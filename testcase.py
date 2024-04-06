ef19a397b25397bc095417bfbea73e156dfca914
Deeksha Goel <goeldeeksha02@gmail.com>
2024-04-06 12:58:42 +0530

    update testcase.py

--- testcase.py
+++ testcase.py
@@ -1,19 +1,15 @@
 from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.service import Service
 
-# URL of your webpage
-URL = 'https://deeksha-goel.github.io/magicwebsite/'
-
-# Initialize Chrome WebDriver
-service = Service(
-    executable_path='C:/Users/scifi/OneDrive/Desktop/chromedriver.exe')
+# Start the WebDriver and open the HTML page
+service = Service(executable_path='/usr/local/bin/chromedriver')
 options = webdriver.ChromeOptions()
 options.add_argument("--headless")
 options.add_argument('--no-sandbox')
 driver = webdriver.Chrome(service=service, options=options)
+
+# URL of your webpage
+URL = 'https://deeksha-goel.github.io/magicwebsite/'
 
 try:
     # Open the webpage
@@ -22,11 +18,11 @@ try:
     # Wait for the h1 element with text containing "Magic Website" to be present
     h1_element = WebDriverWait(driver, 10).until(
         EC.presence_of_element_located(
-            (By.XPATH, "//h1[contains(., 'Simple Website')]"))
+            (By.XPATH, "//h1[contains(., 'Magic Website')]"))
     )
 
     # If the element is found, print success message
-    print("Test Passed: Found h1 tag containing 'Simple Website'")
+    print("Test Passed: Found h1 tag containing 'Magic Website'")
 except Exception as e:
     # If the element is not found, print failure message
     print("Test Failed:", e)
